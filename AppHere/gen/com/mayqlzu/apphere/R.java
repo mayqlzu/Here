02b7c3d906149bee96b262b29938786c9ad40715
Owner <Owner@Owner-THINK>
2013-02-24 14:15:39 +0800

    add UI components in members fragment;

--- AppHere/gen/com/mayqlzu/apphere/R.java
+++ AppHere/gen/com/mayqlzu/apphere/R.java
@@ -28,12 +28,19 @@ public final class R {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
+        public static final int add_members_from_phonebook=0x7f04000e;
+        public static final int add_one_by_hand=0x7f04000c;
         public static final int app_name=0x7f040000;
         public static final int broadcast=0x7f040005;
         public static final int call_the_roll=0x7f040006;
+        public static final int delete_all=0x7f040008;
+        public static final int delete_selected=0x7f040009;
+        public static final int find_contacts_from_sms=0x7f04000d;
         public static final int hello_world=0x7f040001;
         public static final int members=0x7f040004;
         public static final int menu_settings=0x7f040002;
+        public static final int name=0x7f04000a;
+        public static final int number=0x7f04000b;
         public static final int other=0x7f040007;
         public static final int title_activity_main=0x7f040003;
     }
