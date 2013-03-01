0c3c2a975ad21ad766cdca34b1e657bed6b265c6
Owner <Owner@Owner-THINK>
2013-03-01 20:29:31 +0800

    add function: read sms out from inbox and filter all candidates;

--- AppHere/src/com/mayqlzu/apphere/CollectContactsFromSMSActivity.java
+++ AppHere/src/com/mayqlzu/apphere/CollectContactsFromSMSActivity.java
@@ -1,7 +1,13 @@
 package com.mayqlzu.apphere;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CollectContactsFromSMSActivity extends Activity{
     @Override
@@ -9,5 +15,36 @@ public class CollectContactsFromSMSActivity extends Activity{
         super.onCreate(savedInstanceState);
         
         setContentView(R.layout.collect_contacts_from_sms_layout);
+        
+        // register callback for button "start"
+        Button btn_start = (Button)this.findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// read inbox
+				// todo: do it in another thread to avoid UI freeze
+				Cursor cursor = getContentResolver().query(Uri.parse("content://sms/inbox"), 
+						null, null, null, null);
+				cursor.moveToFirst();
+				if(0 == cursor.getCount())
+					return;
+				ArrayList<SMS> msgs = new ArrayList<SMS>();
+				do{
+					// todo: can not handle Chinese now, "ÂíÓÂÇ¿" will be "???" in inbox; solve it later
+				       String address = cursor.getString(cursor.getColumnIndex("address"));
+				       String body = cursor.getString(cursor.getColumnIndex("body"));
+				       String date_sent = cursor.getString(cursor.getColumnIndex("date_sent"));
+				       
+				       System.out.println("body:" + body + " address:" + address + " date_sent:" + date_sent);
+				       
+				       // how can i filter the bodies that are name ?
+				       // for now, just filter the ones that has short length and not contain ?, leave user to filter again
+				       if(body.length() < 10 && !body.contains("?")){
+				    	  msgs.add(new SMS(address, body, date_sent));
+				       }
+				       System.out.println(msgs.size() + " valid msgs found");
+				}while(cursor.moveToNext());
+			}
+		});
     }
 }
---
+++ AppHere/src/com/mayqlzu/apphere/SMS.java
@@ -0,0 +1,13 @@
+package com.mayqlzu.apphere;
+
+public class SMS {
+	public String m_address;
+	public String m_body;
+	public String m_date_sent;
+	
+	public SMS(String a, String b, String d){
+		m_address = a;
+		m_body = b;
+		m_date_sent = d;
+	}
+}
